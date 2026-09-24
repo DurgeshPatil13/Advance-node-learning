deb9a8fb8a805dab6606e5d3f1726f22a0e86b18
DurgeshPatil13 <durgeshpatil0513@gmail.com>
2026-09-24 23:08:45 +0530

    working

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express=require("express");
-const router=express();
+const router=express()
 const validation=require("../middlewares/validate")
-const postdata=require("../controller/controller");
+const postdata=require("../controller/controller")
 const registerscehma=require("../zod/zod");
-router.post("/register",validation(registerscehma),postdata);
+router.post("/register",validation(registerscehma),postdata)
 module.exports=router;
